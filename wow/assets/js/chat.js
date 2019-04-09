78eb1d51f8affdc271eb0bb97115363f37dfb0b1
brian <briancjkim91@gmail.com>
2019-04-09 23:27:02 +1000

    added chat function

---
+++ wow/assets/js/chat.js
@@ -0,0 +1,73 @@
+// make connection from client
+const url = process.env.PRODUCTION
+  ? "https://utubeclone.herokuapp.com/"
+  : "http://localhost:4000";
+const socket = io.connect("http://localhost:4000");
+const chatButton = document.querySelector(".chat-button-container");
+const closeButton = document.querySelector(".chat-close");
+const sendButton = document.querySelector(".sendMessage");
+const handle = document.querySelector(".handle");
+const message = document.querySelector(".message");
+const chat = document.querySelector(".chat");
+const output = document.querySelector(".output");
+const feedback = document.querySelector(".feedback");
+const chatWindow = document.querySelector(".chat-window");
+
+const handleChatButtonClick = () => {
+  chat.classList.toggle("active");
+};
+
+const handleCloseButtonClick = () => {
+  chat.classList.toggle("active");
+};
+
+const handleSendButtonClick = () => {
+  if (message.value !== "") {
+    socket.emit("chat", {
+      handle: handle.value,
+      message: message.value
+    });
+    message.value = "";
+  }
+};
+
+const handleMessageTyping = e => {
+  socket.emit("typing", handle.value);
+  if (e.keyCode === 13) {
+    if (message.value !== "") {
+      e.preventDefault();
+      sendButton.click();
+    }
+  }
+};
+
+// chat window scroll to bottom
+const updateScroll = () => {
+  chatWindow.scrollTop = chatWindow.scrollHeight;
+};
+
+function init() {
+  chatButton.addEventListener("click", handleChatButtonClick);
+  closeButton.addEventListener("click", handleCloseButtonClick);
+  sendButton.addEventListener("click", handleSendButtonClick);
+  message.addEventListener("keypress", handleMessageTyping);
+
+  // socket event Listeners
+  socket.on("chat", data => {
+    feedback.innerHTML = "";
+    const newElement = `<p>
+    <strong>${data.handle}</strong>
+    ${data.message}
+  </p>`;
+    output.innerHTML += newElement;
+    // fix scroll to the bottom
+    updateScroll();
+  });
+  socket.on("typing", data => {
+    feedback.innerHTML = `<p><em>${data} is typing...</em></p>`;
+  });
+}
+
+if (chatButton) {
+  init();
+}
--- wow/init.js
+++ wow/init.js
@@ -2,6 +2,7 @@ import "@babel/polyfill";
 import dotenv from "dotenv";
 import "./db";
 import app from "./app";
+import socket from "socket.io";
 
 dotenv.config();
 
@@ -12,4 +13,16 @@ import "./models/User";
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`listening to ${PORT}`);
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+
+// socket setup and pass server
+const io = socket(server);
+io.on("connection", clientSocket => {
+  console.log("made socket connection", clientSocket.id);
+  clientSocket.on("chat", data => {
+    io.sockets.emit("chat", data);
+  });
+  clientSocket.on("typing", data => {
+    clientSocket.broadcast.emit("typing", data);
+  });
+});
